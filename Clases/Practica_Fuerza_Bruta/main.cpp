24190b6338067f5fc10ccf29ab4c9218d25c9c3e
Antohn <antonio.rios.ch@gmail.com>
2026-04-04 19:16:50 -0500

    add

--- Clases/Practica_Fuerza_Bruta/main.cpp
+++ Clases/Practica_Fuerza_Bruta/main.cpp
@@ -30,7 +30,7 @@ int main() {
 
     n=8;
     for (int i=0;i<n-1;i++) {
-        for (int j=0;j<=n-1-i;j++) {
+        for (int j=0;j<n-1-i;j++) {
             if (arrA[j+1]<arrA[j]) {
                 temp=arrA[j];
                 arrA[j]=arrA[j+1];
